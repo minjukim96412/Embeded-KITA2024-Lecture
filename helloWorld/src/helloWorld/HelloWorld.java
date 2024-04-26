6fbeaca6ae913e4696b2136bababc393cc2031e8
minjukim96412 <168044333+minjukim96412@users.noreply.github.com>
2024-04-26 09:20:21 +0900

    다시

--- helloWorld/src/helloWorld/HelloWorld.java
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -4,10 +4,6 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-<<<<<<< HEAD
-		//ffff
-=======
-		//³ëÆ®ºÏÀ¸·Î ÇÑ¹ø´õ ¼öÁ¤ÇÕ´Ï´Ù.
->>>>>>> 00a2960a4597a5d39004c14d08f08a5f3283b6be
+//	ì•ˆë…•
 	}
 }
