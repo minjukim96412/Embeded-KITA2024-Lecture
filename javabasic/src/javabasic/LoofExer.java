1f15db9eb090a165750575e272a2cbd74fafaf82
minjukim96412 <168044333+minjukim96412@users.noreply.github.com>
2024-04-29 08:49:03 +0900

    다시

--- javabasic/src/javabasic/LoofExer.java
+++ javabasic/src/javabasic/LoofExer.java
@@ -98,7 +98,7 @@ public class LoofExer {
 		//*****
 		System.out.println();
 		
-		int keyNum = 4;
+		int keyNum = 10;
 		
 		for (int i = 0; i < keyNum+1; i++) {
 			for (int j = 0; j < keyNum+1; j++) {
@@ -141,21 +141,25 @@ public class LoofExer {
 		// ***	
 		//  *
 		System.out.println();
+
 		
 		for (int i = 0; i < keyNum+1; i++) {
 			for (int j = 0; j < keyNum+1; j++) {
-				if (i-j == 2 || j-i == 2 || i+j ==0 || i==j) {
-					System.out.print("*");
-				}else {
-					System.out.print(" ");
-				}	
-			}
-			System.out.println();
-		}
-		
-		
-//		12
-//	21      23
-//		32
+				
+					if(i+j < keyNum/2) {
+						System.out.print(" ");
+					}else if (j-i > keyNum/2){
+							System.out.print(" ");
+					}else if (i-j > keyNum/2) {
+							System.out.print(" ");
+					} else if(i+j > keyNum+(keyNum/2)){
+							System.out.print(" ");
+					}else {
+							System.out.print("*");
+						}	
+				}
+				System.out.println();	
+				}
 	}
 }
+
